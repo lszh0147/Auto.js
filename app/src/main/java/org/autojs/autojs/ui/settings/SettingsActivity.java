316ce312ba7122daa2a54da074943982f6bc7eb8
Lszh0147 <no@email.com>
2020-06-30 18:43:15 +0800

    去广播权限 去错误报告

--- app/src/main/java/org/autojs/autojs/ui/settings/SettingsActivity.java
+++ app/src/main/java/org/autojs/autojs/ui/settings/SettingsActivity.java
@@ -18,7 +18,7 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.autojs.autojs.R;
 import org.autojs.autojs.ui.BaseActivity;
-import org.autojs.autojs.ui.error.IssueReporterActivity;
+//import org.autojs.autojs.ui.error.IssueReporterActivity;
 import org.autojs.autojs.ui.update.UpdateCheckDialog;
 
 import java.util.ArrayList;
@@ -104,7 +104,7 @@ public class SettingsActivity extends BaseActivity {
                     .put(getString(R.string.text_theme_color), () -> selectThemeColor(getActivity()))
                     .put(getString(R.string.text_check_for_updates), () -> new UpdateCheckDialog(getActivity())
                             .show())
-                    .put(getString(R.string.text_issue_report), () -> startActivity(new Intent(getActivity(), IssueReporterActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)))
+//                    .put(getString(R.string.text_issue_report), () -> startActivity(new Intent(getActivity(), IssueReporterActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)))
                     .put(getString(R.string.text_about_me_and_repo), () -> startActivity(new Intent(getActivity(), AboutActivity_.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)))
                     .put(getString(R.string.text_licenses), () -> showLicenseDialog())
                     .build();
